28363abf1540cd270146fc7fe12f594d137ab889
Joshua Wiens <joshuaw@easymetrics.com>
2017-07-07 01:52:54 -0500

    test: Add nested order by id test

---
+++ test/cases/chunk-modules-nested-ordered-by-id/webpack.config.js
@@ -0,0 +1,10 @@
+import ExtractTextPlugin from '../../../src/index';
+
+module.exports = {
+  entry: './index',
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'file.css',
+    }),
+  ],
+};
